0e2db66441f3649231cf935fe0ffa6f8ee539ffa
Felipe Costa da Silva <felipe95176@gmail.com>
2024-09-25 11:38:05 -0300

    Falta responsividade na navBar e no createNewTaskPage

--- src/pages/TaskListPage.ts
+++ src/pages/TaskListPage.ts
@@ -1,47 +1,28 @@
 import { createNewTaskPage } from "./CreateTaskPage.js";
-
+import createNavBar from "../components/NavBar.js";
+import showTasksList from "../components/TaskList.js";
+import { tasks, loadTasksFromLocalStorage } from "../utils/storage.js";
 
 export default function showHomePage() {
     window.location.hash = '#home';
+    loadTasksFromLocalStorage();
 
     const toDoContainer = (document.getElementById("to-do-container") as HTMLDivElement);
 
     toDoContainer.innerHTML = '';
-
-    const navBar = document.createElement("nav");
-    navBar.id = "navBar";
-    
-    const navBar_title = document.createElement("h1");
-    navBar_title.id = "navBar_title";
-    navBar_title.innerHTML = "Lista de Tarefas";
-
-    const buttonMenu = document.createElement("button");
-    buttonMenu.type = "button";
-    buttonMenu.id = "button-menu";
-
-    const buttonContainer = document.createElement("div");
-    buttonContainer.id = "button-container";
-
-    function createNewSquare() {
-        const buttonSquares = (document.createElement("div"));
-        buttonSquares.classList.add("button-squares");
-        return buttonSquares;
-    }
-
-    buttonContainer.appendChild(createNewSquare());
-    buttonContainer.appendChild(createNewSquare());
-    buttonContainer.appendChild(createNewSquare());
-    buttonContainer.appendChild(createNewSquare());
 
     const buttonNewTask = document.createElement("button");
     buttonNewTask.type = "button";
     buttonNewTask.id = "button-newTask";
     buttonNewTask.innerHTML = "+";
 
-    buttonMenu.append(buttonContainer);
-    navBar.append(navBar_title, buttonMenu)
-    toDoContainer.append(navBar, buttonNewTask);
-
+    toDoContainer.append(createNavBar());
+    
+    tasks.forEach((task) => {
+        toDoContainer.append(showTasksList(task));  // Passa a tarefa para o componente
+    });
+    
+    toDoContainer.append(buttonNewTask);
     (document.getElementById("button-newTask") as HTMLElement).addEventListener("click", () => {
         createNewTaskPage();
     });
